45195a90472f9ddc018fa612a4da89d0ca7fabef
egorchernook <egor.chernook@yandex.ru>
2023-10-14 14:56:38 +0600

    Base enablement

--- ar_museum/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ar_museum/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_archive
 import geolocator_apple
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterArchivePlugin.register(with: registry.registrar(forPlugin: "FlutterArchivePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
